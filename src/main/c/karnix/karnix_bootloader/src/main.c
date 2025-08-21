0c25af660caf974369c49e4e72ba60221f65bdc5
Ruslan Zalata <rz@fabmicro.ru>
2025-08-21 21:50:51 +0000

    karnix_bootloader: Modify to output messages both to UART0 and to CGA video using xprintf()

--- src/main/c/karnix/karnix_bootloader/src/main.c
+++ src/main/c/karnix/karnix_bootloader/src/main.c
@@ -22,10 +22,21 @@
 #define	MAGIC		0x12300013	// Magic number to check for
 
 // Where in NOR flash to look for applications, defines offsets relative to QSPI_MEMORY_ADDRESS 
-uint32_t app_offsets[] = {0xe0000, 0x100000, 0x200000, 0x300000, 0x400000, 0x500000,
+const uint32_t app_offsets[] = {0xe0000, 0x100000, 0x200000, 0x300000, 0x400000, 0x500000,
 			 0x600000, 0x700000, 0x800000, 0x900000, 0xa00000, 0xb00000,
-			 0xc00000, 0xd00000, 0xe00000, 0xf00000};
+			 0xc00000, 0xd00000, 0xe00000, 0xf00000
+};
 
+// Default RGB palette for text mode
+const uint32_t rgb_palette[16] = {
+			0x00000000, 0x000000f0, 0x0000f000, 0x00f00000,
+			0x0000f0f0, 0x00f000f0, 0x00f0f000, 0x00f0f0f0,
+			0x000f0f0f, 0x000f0fff, 0x000fff0f, 0x00ff0f0f,
+			0x000fffff, 0x00ff0fff, 0x00ffff0f, 0x00ffffff,
+};
+
+extern void __sinit(void *); /* LIBC init function */
+extern unsigned int _IMPURE_DATA; /* LIBC context structure */
 extern unsigned int trap_entry; /* Trap entry point provided by crt.S */
 
 /* Context saving structure */
@@ -38,19 +49,40 @@ struct _context {
 	uint32_t mtval; /* access address causing the trap   */
 } context;
 
+char *BOOTLOADER = "Bootloader";
+
 int main(void) {
 
-	PLIC->ENABLE = 0; /* Disable all PLIC IRQ lines, jsut in case */
-
 begin:
+
+	csr_clear(mstatus, MSTATUS_MIE); // Disable Machine interrupts during hardware init
+	PLIC->ENABLE = 0; /* Disable all PLIC IRQ lines, jsut in case */
 
 	/* Wait for CPU clocks to settle, needed only if running stand-alone. */
 	delay_us(2000000);
 
+	// Initialize CGA video
+
+	// Set text mode and load color palette
+	cga_set_video_mode(CGA_MODE_TEXT);
+
+	// Load default color palette
+	cga_set_palette((uint32_t*)rgb_palette);
+
+	// Clear video framebuffer
+	memset(CGA->FB, 0, CGA_FRAMEBUFFER_SIZE);
+	cga_set_cursor_xy(0, 0);
+	cga_set_scroll(0);
+
 	/* Save current stack pointer for trap handling. */
 	asm volatile ("sw sp, (%0)" :  : "r"(&context.sp));
 
-	printk("\r\nKarnix SoC Bootloader, build #%d on %s at %s\r\n", BUILD_NUMBER, __DATE__, __TIME__);
+	// Initialize LIBC
+	init_sbrk(NULL, 0); // Initialize heap for malloc to use on-chip RAM
+	__sinit(&_IMPURE_DATA); // Init LIBC impure_data structure
+
+	xprintf("[%s] Karnix SoC Bootloader, build #%d on %s at %s\r\n",
+		BOOTLOADER, BUILD_NUMBER, __DATE__, __TIME__);
 
 
 	for(int i = 0; i < APPS_NUM; i++) {
@@ -58,12 +90,13 @@ begin:
 		uint32_t (*long_jump)(void) = (uint32_t (*)(void)) app;
 
 		if(*app == MAGIC) {
-			printk("Calling application at %p\r\n\r\n", app);
-			return long_jump();
+			xprintf("[%s] calling application at %p\r\n\r\n", BOOTLOADER, app);
+			delay_us(2000000);
+			long_jump();
 		}
 	}
 
-	printk("No apps found in Flash!\r\n");
+	xprintf("[%s] No more apps found in Flash!\r\n\r\n", BOOTLOADER);
 
 	goto begin;
 
@@ -93,7 +126,7 @@ void crash(int cause) {
 		      addi t1,t1,16; \
 	      	      sw t1, (%0)" :  : "r"(&context.sp)); 
 
-	printk("\r\n*** BOOT TRAP: %p at %p = %p, mtval = %p\r\n"
+	xprintf("\r\n*** BOOT TRAP: %p at %p = %p, mtval = %p\r\n"
 	       "*** BOOT CONTEXT: sp = %p, gp = %p, tp = %p, heap_end = %p\r\n",
 		cause, context.mepc, *(uint32_t*)(context.mepc & 0xfffffffc),
 		context.mtval, context.sp, context.gp, context.tp,
