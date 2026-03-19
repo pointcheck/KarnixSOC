593acac917caa2b0181cb6a9cd5f1408c9c3ef9a
Ruslan Zalata <rz@fabmicro.ru>
2026-03-19 22:15:31 +0000

    karnix, cga: Fix scrolling issue when delay is zero

--- src/main/c/karnix/karnix_soc/src/cga.c
+++ src/main/c/karnix/karnix_soc/src/cga.c
@@ -541,7 +541,7 @@ void cga_text_print(uint8_t *framebuffer, int x, int y, int fg_color, int bg_col
 		x = x % CGA_TEXT_WIDTH;
 	}
 
-	if(scroll_delay && y >= CGA_TEXT_HEIGHT) {
+	if(y >= CGA_TEXT_HEIGHT) {
 		uint32_t *fb = ((uint32_t*) framebuffer) + (y+1) * CGA_TEXT_WIDTH;
 
 		for(int j = 0; j < CGA_TEXT_WIDTH; j++)
